81ef4252ea2ab8f8a39cc832f609763a3baaea09
libo <lowoo@Lowoos-MacBook-Pro.local>
2014-03-04 14:12:44 +0800

    Initial Commit

---
+++ 设计模式/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  设计模式
+//
+//  Created by libo on 14-3-4.
+//  Copyright (c) 2014年 Lowoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 设计模式/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  设计模式
+//
+//  Created by libo on 14-3-4.
+//  Copyright (c) 2014年 Lowoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
